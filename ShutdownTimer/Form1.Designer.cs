ad49686b7f8b36a2acc644c8cd4f03e4f915351e
Spy7Dragon <bh7y@virginia.edu>
2015-12-21 21:53:21 -0500

    Icon
    Added tray and form icon.

--- ShutdownTimer/Form1.Designer.cs
+++ ShutdownTimer/Form1.Designer.cs
@@ -222,6 +222,7 @@
             this.Controls.Add(this.txtHour);
             this.Controls.Add(this.lblShutdownTime);
             this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Shutdown Timer";
